e047c40b2da0c5f633e05410cfa3418fc9cdf5e9
Nathanim1919 <nathanim2tadele@gmail.com>
2024-12-08 11:36:22 +0300

    configure frontend folder structure

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nabar: React.FC = () => {
+  return (
+    <div>
+      <div className="logo">
+        <h1>Care-Assist</h1>
+      </div>
+
+      <div>
+        <div className="w-10 h-10 rounded-full bg-gray-300"></div>
+        <div>
+            <h3>Nathan Tadele</h3>
+            <p>Doctor</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nabar;
